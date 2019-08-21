c7866f01f7c3d3efb4ca5443b5a14ac0d74651a5
haganssd <54368754+haganssd@users.noreply.github.com>
2019-08-21 12:15:25 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/********************************************************
+* Java Program Name
+*********************************************************
+* Project/Program description
+* ______________________________________
+* Student First Name Last Name
+* Date
+* Course Number and Section
+*********************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name: Shaquan' Hagans");
+		System.out.println("Java Project Name: Hello World");
+		System.out.println("Project Description");
+		System.out.println("Version Date: 1.8.0_221");
+		System.out.println("Course NUmber: CMSC 255-004");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+	printHeading();
+	System.out.println("Hello");
+	}
+}
