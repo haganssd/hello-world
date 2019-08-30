bd9893cb641d51ba533bcd720771eef4cbcbca09
haganssd <54368754+haganssd@users.noreply.github.com>
2019-08-29 22:46:52 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,44 @@
+/********************************************************************
+* Java program name: NameArt.java
+*********************************************************************
+* Project description
+* This project outputs my name in a unique way on the display screen
+* Name: Shaquan' Hagans
+* Version Date: jdk 1.8.0_222
+* Course Number and Section: CMSC 255-004
+*********************************************************************/
+
+public class NameArt {
+	public static void main(String[] args) {
+		System.out.println(" Shaquan Hagans");
+		System.out.println(" Project 1 - NameArt.java");
+		System.out.println(" This project outputs my name in a unique way on the display screen");
+		System.out.println(" August 26,2019");
+		System.out.println(" CMSC 255-004");
+		System.out.println("        ________________________________________________");
+		System.out.println("       /  ____________________________________________  \\");
+		System.out.println("      |  /                                            \\  \\");
+		System.out.println("      | | *   *  ***** *     *      ****               | |");
+		System.out.println("      | | *   *  *     *     *     *    *              | |");
+		System.out.println("      | | *****  ***** *     *     *    *              | |");
+		System.out.println("      | | *   *  *     *     *     *    *              | |");
+		System.out.println("      | | *   *  ***** ***** *****  ****               | |");
+		System.out.println("      | |                                              | |");
+		System.out.println("      | | ***** *   *  ***   ***  *   *  ***  *   *  * | |");
+		System.out.println("      | | *     *   * *   * *   * *   * *   * **  *  * | |");
+		System.out.println("      | | ***** ***** ***** *   * *   * ***** * * *  * | |");
+		System.out.println("      | |     * *   * *   * *   * *   * *   * *  **    | |");
+		System.out.println("      | | ***** *   * *   *  *** * ***  *   * *   *  * | |");
+		System.out.println("      | |                                              | |");
+		System.out.println("      | |                                              | |");
+		System.out.println("      \\  \\_____________________________________________/ /");
+		System.out.println("       \\________________________________________________/");
+		System.out.println("      _______________________________________________________");
+		System.out.println("     / [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] \\  ");
+		System.out.println("    / [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] []\\");
+		System.out.println("   / [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] []  \\");
+		System.out.println("  / [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] [] \\");
+		System.out.println(" / [] [] [] [] [_____________________________ ] [] [] [] [] [] []\\");
+		System.out.println("(_________________________________________________________________)");
+	}
+}
