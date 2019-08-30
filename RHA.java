b98bfaf5a25b52bd36e5cfe1ec22d968ce7c5b85
haganssd <54368754+haganssd@users.noreply.github.com>
2019-08-29 22:46:35 -0400

    Add files via upload

---
+++ RHA.java
@@ -0,0 +1,12 @@
+public class RHA {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println(" Program Name: RHA Communnity Council");
+		System.out.println(" Position Applying For: Director of Records");
+		System.out.println(" Name: Shaquan Hagans");
+		System.out.println(" Major: Psychology w/ concentration in Applied Psychology");
+		System.out.println(" Minor: Computer Science");
+		System.out.println(" Election Process: Aug. 30th - Sept. 2");
+		System.out.println("");
+	}
+}
